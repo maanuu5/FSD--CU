81950a1bd9dbd34b53e3a10cd25d99fc081e0f91
Manu <manu.crumbled@gmail.com>
2026-09-02 10:34:07 +0530

    unit-2-easy

---
+++ Unit-2/1.1/fs.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    fs.readFile("manu.txt","utf-8",(err,data)=>{
+        res.send(data)
+        if(err){
+            res.json({
+                msg:err
+            })
+        }
+    })
+})
+
+app.listen(4000)
