f2f266f6349a51fa4bb28ddab8d919ac71bea427
Manu <manu.crumbled@gmail.com>
2026-09-09 10:33:40 +0530

    cli

---
+++ Unit-2/1.1/medium.js
@@ -0,0 +1,25 @@
+// step1 = create a file 
+// step2 = write fn as per your requirements -- calculator 
+
+function add(a,b){
+    return a+b
+}
+
+function sub(a,b){
+    return a-b
+}
+
+function div(a,b){
+    return a/b
+}
+
+function mul(a,b){
+    return a*b
+}
+
+module.exports = {
+    add,
+    sub,
+    div,
+    mul,
+}
